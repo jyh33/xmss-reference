8cf7abe6abfd724559823bbd36330b290d26bdb6
jyh33 <jyh33@mail.ustc.edu.cn>
2024-11-28 17:04:38 +0800

    11.28.down1

--- threshold.c
+++ threshold.c
@@ -3,5 +3,20 @@
 #include "params.h"
 #include "threshold.h"
 
+/*建立每个份额的随机数
+*/
+void threshold_key_init(unsigned char sk,unsigned char *ts_sk, const uint32_t oid){
+    xmss_params params;
+    unsigned int i;
 
-void threshold_key_init(); //建立每个份额的随机数
+    if (xmss_parse_oid(&params, oid)) {
+        return -1;
+    }
+    for (i = 0; i < XMSS_OID_LEN; i++) {
+        /* For an implementation that uses runtime parameters, it is crucial
+        that the OID is part of the secret key as well;
+        i.e. not just for interoperability, but also for internal use. */
+        sk[XMSS_OID_LEN - i - 1] = (oid >> (8 * i)) & 0xFF;
+    }
+    return xmss_core_keypair(&params, pk + XMSS_OID_LEN, sk + XMSS_OID_LEN);
+} 
